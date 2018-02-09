44a56fb01a0be29699b8b2193cee7cec8923b836
andres.cuervo <andres.cuervo@tecnilogica.com>
2018-02-09 14:12:18 +0100

    Add layout

--- resources/views/tracks/ranking.blade.php
+++ resources/views/tracks/ranking.blade.php
@@ -1,7 +1,34 @@
-<ul>
-    @foreach($tracksInfo->tracks as $a_trackInfo)
+@extends('layouts.layout')
 
-        <li>{{ $a_trackInfo->name }} -- {{ $a_trackInfo->album->name }} -- {{ $a_trackInfo->artists[0]->name }}</li>
-    @endforeach
-</ul>
+@section('body')
+
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Id</th>
+            <th scope="col">Song</th>
+            <th scope="col">Album Name</th>
+            <th scope="col">Artists</th>
+            <th scope="col">Reproductions</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach($tracksInfo->tracks as $indexKey => $a_trackInfo)
+            <tr>
+                <th scope="row">{{ $indexKey }}</th>
+                <td>{{ $a_trackInfo->id }}</td>
+                <td>{{ $a_trackInfo->name }}</td>
+                <td>{{ $a_trackInfo->album->name }}</td>
+                <td>{{ $a_trackInfo->artists[0]->name }}</td>
+                <td>{{ $a_trackInfo->reproductions }}</td>
+            </tr>
+        @endforeach
+
+        </tbody>
+    </table>
+
+@endsection
 
