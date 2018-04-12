531b1417bb2e5ec72acf2d88fc294eed86c9890d
andres.cuervo <andres.cuervo@tecnilogica.com>
2018-04-12 16:37:35 +0200

    WIP

--- resources/views/posterdigital/tops.blade.php
+++ resources/views/posterdigital/tops.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
     <div id="app">
-        <h2> @{{ title }} </h2>
+        <h2 class="logo" />
+        <h2 v:if="title" class="offset-md-1"> @{{ title }} </h2>
         <div class="container-fluid" v-if="results.length">
             <div class="col-results col-md-12">
                 <div class="row">
